e6916deb7e33d0d229eec8f760f8024f285090bb
Hayat Tamboli <hayat.tamboli@gmail.com>
2024-04-04 17:38:01 +0530

    to connect supabase with unity

---
+++ Assets/SupabaseInitiation.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using Supabase;
+using Postgrest.Models;
+using static Supabase.Realtime.PostgresChanges.PostgresChangesOptions;
+using Unity.VisualScripting;
+
+
+[Postgrest.Attributes.Table("text-copy-from-unity")]
+class MyMessage : BaseModel
+{
+
+    [Postgrest.Attributes.Column("message")]
+    public string Message { get; set; }
+}
+
+
+[Postgrest.Attributes.Table("rename-folder-in-app")]
+class RenameMessage : BaseModel
+{
+
+    [Postgrest.Attributes.Column("folder_name")]
+    public string Rename { get; set; }
+}
+
+public class SupabaseInitiation : MonoBehaviour
+{
+
+    public Supabase.Client supabase;
+    public string renameName;
+    void Start()
+    {
+        var options = new Supabase.SupabaseOptions
+        {
+            AutoConnectRealtime = true
+        };
+        supabase = new Supabase.Client("https://ibmzlysxftqqfosreljy.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlibXpseXN4ZnRxcWZvc3JlbGp5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTE4MDU0NjksImV4cCI6MjAyNzM4MTQ2OX0.OeXAAkPq_wGgDLlAtpORjIuBgUaXtNv5qaWw4_p_YUk", options);
+        initializeSupabase();
+        Debug.Log("Supabase Initialized");
+        fetchData();
+        // insertData();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    async void initializeSupabase()
+    {
+        await supabase.InitializeAsync();
+        Debug.Log("Initializing Supabase");
+    }
+
+    async void insertData()
+    {
+        var model = new MyMessage
+        {
+            Message = "The Shire"
+        };
+        await supabase.From<MyMessage>().Insert(model);
+    }
+
+    async void fetchData()
+    {
+
+        //await supabase.From<RenameMessage>().On(ListenType.Inserts, (sender, change) =>
+        //{
+        //  Debug.Log(change.Payload.Data);
+        //});
+
+        var channel = supabase.Realtime.Channel("realtime", "public", "rename-folder-in-app", "folder_name");
+
+        channel.AddPostgresChangeHandler(ListenType.Inserts, async (sender, change) =>
+        {
+            // whenever a new row is inserted in the table, this code will be executed
+            var result = await supabase.From<RenameMessage>().Get();
+            renameName = result.Models[result.Models.Count - 1].Rename;
+            Debug.Log("Renamed Folder: " + renameName);
+        });
+
+        await channel.Subscribe();
+    }
+}
